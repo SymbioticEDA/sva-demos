0652a61df64d1799c412c57b810473bfccaa8b1c
Claire Wolf <claire@symbioticeda.com>
2020-04-21 16:06:22 +0200

    Update
    Signed-off-by: Claire Wolf <claire@symbioticeda.com>

--- demo_01_impl.sv
+++ demo_01_impl.sv
@@ -2,9 +2,9 @@ module demo_01_impl (input clock);
 	wire reset;
 	wire a, b;
 
-	seq #("-_____________________") seq_r (clock, reset);
-	seq #("_-____________________") seq_a (clock, a);
-	seq #("__-___________________") seq_b (clock, b);
+	seq #("-_____-____-__________") seq_r (clock, reset);
+	seq #("_-____-___-______-____") seq_a (clock, a);
+	seq #("__-___-__________-____") seq_b (clock, b);
 
 	default clocking @(posedge clock); endclocking
 	default disable iff (reset);
