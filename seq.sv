f04d6fa2ef519f709b24656f4ca2407eaca4f53a
Claire Wolf <claire@symbioticeda.com>
2020-05-12 17:47:16 +0200

    Fix Verific warnings
    Signed-off-by: Claire Wolf <claire@symbioticeda.com>

--- seq.sv
+++ seq.sv
@@ -1,41 +1,45 @@
 `default_nettype none
 module seq #(
-	parameter [1023:0] seq = ""
+	parameter [1023:0] seq = "",
+	parameter integer N = 1
 ) (
 	input wire clock,
-	output reg [3:0] dout
+	output wire [N-1:0] dout
 );
 	localparam seqlen = $clog2(seq) / 8;
 
 	integer cycle = 0;
 	wire [31:0] position = seqlen - cycle;
-	wire [7:0] ch = seq >> (8*position);
+	wire [7:0] ch = (seq >> (8*position)) & 255;
 
 	always @(posedge clock) begin
 		cycle <= cycle + 1;
 	end
 	
+	reg [3:0] dout_reg;
+	assign dout = dout_reg[N-1:0];
+
 	always @* begin
-		dout = 4'b xxxx;
+		dout_reg = 4'b xxxx;
 		case (ch)
-			"0", "_": dout = 0;
-			"1": dout = 1;
-			"2": dout = 2;
-			"3": dout = 3;
-			"4": dout = 4;
-			"5": dout = 5;
-			"6": dout = 6;
-			"7": dout = 7;
-			"8": dout = 8;
-			"9": dout = 9;
-			"A", "a": dout = 10;
-			"B", "b": dout = 11;
-			"C", "c": dout = 12;
-			"D", "d": dout = 13;
-			"E", "e": dout = 14;
-			"F", "f", "-": dout = 15;
-			"X", "x": dout = 4'b xxxx;
-			"Z", "z": dout = 4'b zzzz;
+			"0", "_": dout_reg = 0;
+			"1": dout_reg = 1;
+			"2": dout_reg = 2;
+			"3": dout_reg = 3;
+			"4": dout_reg = 4;
+			"5": dout_reg = 5;
+			"6": dout_reg = 6;
+			"7": dout_reg = 7;
+			"8": dout_reg = 8;
+			"9": dout_reg = 9;
+			"A", "a": dout_reg = 10;
+			"B", "b": dout_reg = 11;
+			"C", "c": dout_reg = 12;
+			"D", "d": dout_reg = 13;
+			"E", "e": dout_reg = 14;
+			"F", "f", "-": dout_reg = 15;
+			"X", "x": dout_reg = 4'b xxxx;
+			"Z", "z": dout_reg = 4'b zzzz;
 		endcase
 	end
 endmodule
